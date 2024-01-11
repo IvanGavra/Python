9ec708a821f17e5f7501757989e84a1ce24f68d7
IvanGavra <ivanbyc137@gmail.com>
2024-01-11 11:47:45 -0600

    Add day 16, and GUI features

---
+++ To_do_app/bonus/bonus16/gui.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+
+lable1 = sg.Text("Select file to compress")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose")
+
+lable2 = sg.Text("DEstination folder")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose")
+compress_button = sg.Button("Compress")
+window = sg.Window("Compressor",
+                   layout=[[lable1, input1, choose_button1],
+                           [lable2, input2, choose_button2],
+                           [compress_button]])
+
+window.read()
+window.close()
